29d46b818c5e3b0ba4622a4b7f36a0786f30b98b
github username <github email address>
2019-07-16 17:31:37 +0100

    application and user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+
+    def get_current_user
+        id = decode_token['id'] #we want to get back the value of the ide from the decoded array that we receive from decode_token
+        User.find_by(id: id)
+    end
+
+
+    #to access tokens that the user is sending the tokens via the headers
+    def token
+        request.headers['Authorisation']
+    end
+    
+    #not the best way to store secret but will work for now 
+    # def secret
+    #     "oursecret"
+    # end
+
+    #encode data
+    def issue_token(data)
+        JWT.encode(data, ENV['FEMHUB_SECRET'])
+    end
+
+    def decode_token
+        begin
+            JWT.decode(token, ENV['FEMHUB_SECRET']).first
+        rescue 
+            {}
+        end
+    end
+
+    #the begin and rescue methods are known as a "try catch". the try catch is saying: try doing this first thing, if this throws an error, do the rescue, this will prevent everything from crashing. 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,59 @@
 class UsersController < ApplicationController
-
-    def create
-        
-        @user = User.create(user_params)
+    def new
+        @user = User.new
+    end
+    
+    def signup
+        @user = User.new(user_params)
         if @user.valid?
-            render json: { user: UserSerializer.new(@user)}, status: :created
+            @user.save
+            render json: @user
         else
-            render json: { error: 'failed to create user'}, status: :not_acceptable
+            render json: { error: 'Please fill out all required fields and try again'}
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user
+        else 
+            render json: { error: 'User not found'}, status: 404
         end
     end
 
+    def signin
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: {username: user.username, first_name: user.first_name, token: issue_token({ id: user.id })} #just sending the stuff that we need, the username to be greeted and the id for local storage
+        else
+            render json: { error: 'Invalid username/password combination'}, status: 401
+        end
+    end
+
+    def validate
+        user = get_current_user
+        if user
+            render json: { username: user.username, first_name: user.first_name, token: issue_token({id: user.id})}
+        else
+            render json: {error: 'User not found'}, status: 404
+            #I don't want this to render all the time.
+        end
+    end
+
+    def destroy
+    end
+
+
     private
     def user_params
-        params.permit(:username, :password, :first_name, :last_name, :photo, :bio)
+        params.require(:user).permit(:username, :password, :first_name, :last_name, :bio, :photo)
     end
+ 
 
 end
