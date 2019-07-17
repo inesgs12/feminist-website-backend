1e0365e87615c90042f907a8f7baaedbfbd9c654
github username <github email address>
2019-07-17 13:55:25 +0100

    Author controller done
    index and show methods done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,11 @@
 class AuthorsController < ApplicationController
+    
+    def index
+        author = Author.all
+    end
+
+    def show
+        author = Author.find_by(id: params[:id])
+    end
+
 end
