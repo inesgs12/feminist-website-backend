04c25c9d501fb8db96bf68e440fc1e8932ccfe8c
Ines Guerrero <inesgs@gmail.com>
2019-07-22 20:11:15 +0100

    like theories

--- app/controllers/favourite_theories_controller.rb
+++ app/controllers/favourite_theories_controller.rb
@@ -1,5 +1,5 @@
 class FavouriteTheoriesController < ApplicationController
-    before_action :find_favourite_theory, only: [ :show, :destroy]
+    before_action :find_favourite_theory, only: [:show]
     before_action :find_theory, only: [:create, :destroy]
     before_action :find_user, only: [:create]
 
@@ -9,23 +9,18 @@ class FavouriteTheoriesController < ApplicationController
     end
 
     def show
-        render json: favourite_theory
+        # render json: favourite_theory
     end 
 
     def create
-        favourite_theory = FavouriteTheory.new(favourite_theory_params)
-        if favourite_theory.valid?
-            favourite_theory.save 
-            render json: favourite_theory
-        else
-            render json: { error: 'Please try again'}
-            #render the explicit error!
-        end
+        favourite_theory = FavouriteTheory.create(favourite_theory_params)
+        render json: favourite_theory
     end
 
     def destroy 
+        favourite_theory = FavouriteTheory.find_by(theory_id: params[:theory_id], user_id: params[:user_id])
         favourite_theory.destroy 
-        render json: theory
+        render json: { message: "author removed from liked authors"}
     end
 
     private
